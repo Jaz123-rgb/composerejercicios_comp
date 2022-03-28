72b51473dfd2eed5ad3018ff33ad6ce727a63cb9
Jaziel Perez <pjaziel723@gmail.com>
2022-03-28 03:13:07 -0600

    update ejer-5 uso de comandos para composer part 1

---
+++ composerejer-5/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0b211c11e2ea691b2aa39a21dab4bd65e9dd4188',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0b211c11e2ea691b2aa39a21dab4bd65e9dd4188',
+            'dev_requirement' => false,
+        ),
+    ),
+);
